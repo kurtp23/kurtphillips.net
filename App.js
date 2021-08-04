c08f3b5e52e73bd75bb13798523652a331edb095
kurtp23 <kurt_phillips@live.com>
2021-08-04 15:59:35 -0700

    next app

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import "./client/app.css";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.h1}>Welcome to KurtPhillips.net</Text>
+      <View style={styles.column}>
+        <Text>where is this1</Text>
+        <Text>where is this3</Text>
+      </View>
+      <View style={styles.column}>
+        <Text>where is this2</Text>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +21,17 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    justifyContent: "center",
+  },
+  h1: {
+    color: "white",
+    fontSize: "50px",
+    verticalAlign: "text-top",
+    textAlign: "center",
+    textShadow: "0px 1px 2px  #000",
+  },
+  column: {
+    flexDirection: "column",
   },
 });
