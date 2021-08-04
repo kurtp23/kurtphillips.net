c3406dc27c55b1245167175b69f6f8bdd8c3e345
kurtp23 <kurt_phillips@live.com>
2021-08-04 16:38:47 -0700

    added some layout

--- App.js
+++ App.js
@@ -4,24 +4,31 @@ import { StyleSheet, Text, View } from "react-native";
 import "./client/app.css";
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.containerColumn}>
       <Text style={styles.h1}>Welcome to KurtPhillips.net</Text>
-      <View style={styles.column}>
-        <Text>where is this1</Text>
-        <Text>where is this3</Text>
+      <View style={styles.containerRow}>
+        <View style={styles.column}>
+          <Text style={styles.p1}>
+            IM not exactly sure the purpose of this website, but hope to use it to improve apon my
+            skills, or just record things that happen in my life.
+          </Text>
+        </View>
+        <View style={styles.column}>
+          <Text style={styles.p1}>this is some other text that i also dont have an idea for</Text>
+        </View>
       </View>
-      <View style={styles.column}>
-        <Text>where is this2</Text>
-      </View>
-
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    display: "flex",
+  containerColumn: {
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  containerRow: {
+    flexDirection: "row",
     justifyContent: "center",
   },
   h1: {
@@ -31,7 +38,15 @@ const styles = StyleSheet.create({
     textAlign: "center",
     textShadow: "0px 1px 2px  #000",
   },
+  p1: {
+    color: "white",
+    fontSize: "20px",
+    textAlign: "left",
+    textShadow: "0px 1px 2px  #000",
+  },
   column: {
     flexDirection: "column",
+    paddingHorizontal: "100px",
+    flexWrap: "wrap",
   },
 });
